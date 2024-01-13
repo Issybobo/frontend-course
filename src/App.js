c74742182533f6717f359283bcb1b0ffd21298dd
Israel Ogunsola <110337578+Issybobo@users.noreply.github.com>
2024-01-13 10:44:17 +0100

    use effect

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Header from './components/Header';
 import Employees from './pages/Employees';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Customers from './pages/Customer';
+import Dictionary from './components/Dictionary';
 
 function App() {
   
@@ -20,6 +21,8 @@ function App() {
       <Route path='/employees' element = {<Employees/>}/>
 
       <Route path = '/customers' element = {<Customers/>}/>
+
+      <Route path='/dictionary' element = {<Dictionary/>}/>
     </Routes>
     </Header>
   </BrowserRouter>
